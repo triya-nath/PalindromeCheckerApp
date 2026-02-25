f19d300037e5e285e9e7d3bc8358285feed83aff
triya-nath <triya.nath@gmail.com>
2026-02-25 09:24:00 +0530

    Triya added UC3

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,22 @@
 public class PalindromeCheckerApp {
-    static void main() {
-        System.out.println("Welcome PalindromeCheckerApp");
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println("The string \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is not a palindrome.");
+        }
     }
 }
