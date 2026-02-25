610cda2e7c02ab3f9a07e1a50a6d35630c99cc21
Lithika-Shri-S <lithikashri1107@gmail.com>
2026-02-25 09:11:19 +0530

    Triya added UC1

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,20 +1,5 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-
-        String input = "madam";
-        boolean isPalindrome = true;
-
-        for(int i = 0; i < input.length() / 2; i++) {
-            if(input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if(isPalindrome) {
-            System.out.println("The string is a palindrome");
-        } else {
-            System.out.println("The string is not a palindrome");
-        }
+    static void main() {
+        System.out.println("Welcome PalindromeCheckerApp");
     }
 }
